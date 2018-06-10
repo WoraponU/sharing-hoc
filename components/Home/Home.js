36b4ef4133ba64b1136621fa65bf5c419a1aadd3
worapon prathuangthip <woraponok@gmail.com>
2018-06-10 14:27:19 +0700

    no message

---
+++ components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>List of item:</h1>
+        <ul>
+          <li>1</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Home
